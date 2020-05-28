6b7b3316b16bbee6790a4117f359b9e99fe2f23f
Eduardo Coronado <e.coronado@duke.edu>
2020-05-28 14:25:48 -0400

    new timeline

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+
+
+$(document).ready(function() {
+
+    $('#professional-timeline').verticalTimeline({
+        startLeft: false,
+        alternate: false,
+        animate: "slide",
+        arrows: false
+    });
+
+    $('#research-timeline').verticalTimeline({
+        startLeft: false,
+        alternate: false,
+        animate: "slide",
+        arrows: false
+    });
+
+    $('#education-timeline').verticalTimeline({
+        startLeft: false,
+        alternate: false,
+        animate: "slide",
+        arrows: false
+    });
+
+    $('#patents-timeline').verticalTimeline({
+        startLeft: false,
+        alternate: false,
+        animate: "slide",
+        arrows: false
+    });
+
+    $('#awards-timeline').verticalTimeline({
+        startLeft: false,
+        alternate: false,
+        animate: "slide",
+        arrows: false
+    });
+
+
+});
